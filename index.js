c41e2b299224105787131b60300a1085ab7f23b9
Caitlin Jordan <jordan.cait17@gmail.com>
2023-01-21 15:15:04 -0800

    completed lab

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback){
+    callback();
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+
+    }
+}
+
+function returnsANamedFunction(){
+    return function pickles(){
+        
+    }
+}
